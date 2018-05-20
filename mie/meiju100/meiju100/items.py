518effaf4de42bb67a85765001554c183740c1f4
xinyan3 <sky831202@gmail.com>
2018-05-21 02:39:29 +0800

    常规提交

--- mie/meiju100/meiju100/items.py
+++ mie/meiju100/meiju100/items.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
@@ -15,4 +9,14 @@ class Meiju100Item(scrapy.Item):
     storyState = scrapy.Field()
     tvStation = scrapy.Field()
     updateTime = scrapy.Field()
+
+class UrlItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field()
+    title=scrapy.Field()
+    time=scrapy.Field()
+    readtimes=scrapy.Field()
+    article_url=scrapy.Field()
+    pass
+   
 
